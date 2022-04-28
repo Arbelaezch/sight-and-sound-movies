f5aebeab4711402bdd3cb8c7aa30fb951194e12c
Arbelaezch <arbelaezch@gmail.com>
2022-04-27 23:47:33 -0600

    added images

--- sight&sound.py
+++ sight&sound.py
@@ -2,10 +2,27 @@ from bs4 import BeautifulSoup
 import requests
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 Bootstrap(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/sight-and-sound.db'
+db = SQLAlchemy(app)
+
+class Movie(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(80), nullable=False)
+	director = db.Column(db.String(50), nullable=False)
+	country = db.Column(db.String(20))
+	year = db.Column(db.String(4), nullable=False)
+	synopsis = db.Column(db.String(300))
+    
+
+	def __repr__(self):
+		return '<Movie %r>' % self.title
+
+
+
 
 
 URL = "https://www.bfi.org.uk/sight-and-sound/greatest-films-all-time"
